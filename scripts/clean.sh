8b4eba150bc3f7658f925070501624ac62b2580b
mmcgarrigle <mmcgarrigle7@icloud.com>
2020-08-05 10:24:43 +0000

    change to clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,5 +5,3 @@ docker service update --image mmcgarrigle/service_2:latest Riddleservice_service
 docker service update --image mmcgarrigle/service_3:latest Riddleservice_service_3
 docker service update --image mmcgarrigle/service_4:latest Riddleservice_service_4
 docker rmi $(docker images -f "dangling=true" -q) -f
-
-docker system prune -f
