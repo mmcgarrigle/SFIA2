6dc6f1d5637ad0a2b559c773b24774fd03a08f5f
mmcgarrigle <mmcgarrigle7@icloud.com>
2020-08-05 12:03:40 +0000

    re-added docker system prune to clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker service update --image mmcgarrigle/service_1:latest Riddleservice_service_1
-docker service update --image mmcgarrigle/service_2:latest Riddleservice_service_2
-docker service update --image mmcgarrigle/service_3:latest Riddleservice_service_3
-docker service update --image mmcgarrigle/service_4:latest Riddleservice_service_4
+# docker service update --image mmcgarrigle/service_1:latest Riddleservice_service_1
+# docker service update --image mmcgarrigle/service_2:latest Riddleservice_service_2
+# docker service update --image mmcgarrigle/service_3:latest Riddleservice_service_3
+# docker service update --image mmcgarrigle/service_4:latest Riddleservice_service_4
+
+docker system prune -f
