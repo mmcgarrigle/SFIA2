cfab5048fb3f1aa463e2a9431dcc6de1a8c20dba
mmcgarrigle <mmcgarrigle7@icloud.com>
2020-08-07 09:34:49 +0000

    finalised web-app functionality to persist data in mysql database and to show last 10 posts in service 1. Also added inventory.cfg file

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -23,15 +23,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
                                         environ.get('MYSQL_DB_NAME')
 
 
-class Riddles(db.Model):
+class riddles(db.Model):
     ID = db.Column(db.Integer, primary_key=True, unique=True)
     Riddle = db.Column(db.String(1000), nullable=False)
    
     def __repr__(self):
         return ''.join(
             [
-                'ID: ' + str(self.id) + '\n' 
-                'Riddle: ' + self.riddle
+                'ID: ' + str(self.ID) + '\n' 
+                'Riddle: ' + self.Riddle
             ]
         )
 
@@ -39,7 +39,10 @@ class Riddles(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     response = requests.get('http://service_4:5003/randomriddle')
+    # response = requests.get('http://localhost:5003/randomriddle')
     print(response)
     sentence = response.text
-    post_data = Riddles.query.all()
-    return render_template('index.html', sentence = sentence, title = 'Home', posts = post_data)
+    post_data = riddles.query.order_by(riddles.ID.desc()).limit(10).all()
+    db.session.add(riddles(Riddle = sentence))
+    db.session.commit()
+    return render_template('index.html', sentence = sentence, title = 'Home', riddles = post_data)
